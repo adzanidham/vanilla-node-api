f09842691b0a9184a7d542b31c5a89f0a3551a5f
adzanidham <adzani234@gmail.com>
2020-10-16 20:09:53 +0700

    create delete product route

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const { getProducts, getProduct, createProduct, updateProduct } = require('./controller/products-controller')
+const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('./controller/products-controller')
 
 const server = http.createServer((req, res) => {
   // set url route
@@ -19,6 +19,11 @@ const server = http.createServer((req, res) => {
   else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
     const id = req.url.split('/')[3]
     updateProduct(req, res, id)
+  }
+  // delete product
+  else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE') {
+    const id = req.url.split('/')[3]
+    deleteProduct(req, res, id)
   }
 
   else {
