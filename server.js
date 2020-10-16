cad974b2fe9634d26e46daca5cd922b6940e5d39
adzanidham <adzani234@gmail.com>
2020-10-16 10:17:41 +0700

    set route

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const http = require('http')
 const products = require('./data/products')
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify(products))
+  // set url route
+  if (req.url === '/api/products') {
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(products))
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ message: 'Route not found' }))
+  }
 })
 
 const PORT = process.env.PORT || 3000
