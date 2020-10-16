cd1d73d80a02bd256afa79de93593b49857a4c2f
adzanidham <adzani234@gmail.com>
2020-10-16 20:01:37 +0700

    create update product route with body request

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http')
-const { getProducts, getProduct, createProduct } = require('./controller/products-controller')
+const { getProducts, getProduct, createProduct, updateProduct } = require('./controller/products-controller')
 
 const server = http.createServer((req, res) => {
   // set url route
@@ -7,13 +7,18 @@ const server = http.createServer((req, res) => {
     getProducts(req, res)
   }
   // select product
-  else if (req.url.match(/\/api\/product\/([0-9]+)/) && req.method === 'GET') {
+  else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
     const id = req.url.split('/')[3]
     getProduct(req, res, id)
   }
   // input product
   else if (req.url === '/api/products' && req.method === 'POST') {
     createProduct(req, res)
+  }
+  // update product
+  else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
+    const id = req.url.split('/')[3]
+    updateProduct(req, res, id)
   }
 
   else {
